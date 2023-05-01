13e1b49e3105bbbe095a88acce1ee5447cdfa6d1
KABORE <kaborep08@gmail.com>
2023-05-01 20:30:40 +0000

    first commit

---
+++ dclic.js
@@ -0,0 +1,10 @@
+// const test = document.querySelector("nav");
+// window.addEventListener("scroll" , () =>{
+//     console.log(window.scrollY);
+//     if (window.scrollY>320) {
+//         test.style.top=0;
+//     }else{
+//         test.style.top="-80px";
+//     }
+
+// })
